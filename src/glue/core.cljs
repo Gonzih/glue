b6720e6b0a45ef8dc45eafc60d325af0cd8a743f
Max Gonzih <gonzih@gmail.com>
2017-11-12 18:03:53 +0100

    Simplify the example

--- src/glue/core.cljs
+++ src/glue/core.cljs
@@ -1,16 +1,17 @@
 (ns glue.core
-    (:require [glue.api :as g]))
+  (:refer-clojure :exclude [atom])
+  (:require [glue.api :as g :refer [atom]]))
 
 (enable-console-print!)
 
-(def counter (g/atom 0))
-(def todos (g/atom ["hello"]))
+(def counter (atom 0))
+(def todos (atom ["hello"]))
 
 (g/defcomponent
   :todo
   {:template "#todo"
-   :state (fn [] {:todos (g/atom ["hello"])
-                  :counter (g/atom 0)})
+   :state (fn [] {:todos   (atom ["hello"])
+                  :counter (atom 0)})
    :methods {:add-random-todo (fn [this state _]
                                 (swap! (:todos state) #(conj % (str (rand-int 100)))))
              :child-clicked (fn [this state n]
@@ -20,7 +21,7 @@
   :todo-item
   {:template "#todo-item"
    :props [:label]
-   :state (fn [] {:counter (g/atom 0)})
+   :state (fn [] {:counter (atom 0)})
    :computed {:counter-label (fn [this state]
                                (str @(:counter state) " clicks"))}
    :methods {:click-me (fn [this state _]
