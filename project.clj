01b9beafee3b070ca28b9feb98d3a1fd721dd58a
Max Gonzih <gonzih@gmail.com>
2017-12-03 16:38:26 +0100

    Bump verson

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject glue "0.1.0-SNAPSHOT"
+(defproject glue "0.1.1-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
