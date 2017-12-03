847398309de57dcf95d83ec81d6043b37fa6647f
Max Gonzih <gonzih@gmail.com>
2017-12-03 16:35:03 +0100

    Fix test runner

--- test/glue/core_test.cljs
+++ test/glue/core_test.cljs
@@ -1,6 +1,15 @@
 (ns ^:figwheel-always glue.core-test
   (:require [cljs.test :refer-macros [run-all-tests]]
+            [glue.api-test]
             [glue.gatom-test]))
+
+(defn exit! [code]
+  (when js/phantom (.exit js/phantom code)))
+
+(defmethod cljs.test/report [:cljs.test/default :end-run-tests] [m]
+  (if (cljs.test/successful? m)
+    (exit! 0)
+    (exit! 1)))
 
 (enable-console-print!)
 
